937b28594815b04a1a023cc306059a52b5831ccd
hari_08 <97215411+hari06021999@users.noreply.github.com>
2023-07-05 06:59:29 +0530

    Update MMTS_APP.ino

--- MMTS_APP.ino
+++ MMTS_APP.ino
@@ -4,21 +4,13 @@
 #define LED D0            // Led in NodeMCU at pin GPIO16 (D0).
 #define WIFI_LED D1
 // WiFi
-const char *ssid = "Ga"; // Enter your WiFi name
-const char *password = "12345678";  // Enter WiFi password
+const char *ssid = "Sri Vinayaka SMP PG 2"; // Enter your WiFi name
+const char *password = "88861564492";  // Enter WiFi password 74140749
 // MQTT Broker
 const char *mqtt_broker = "broker.hivemq.com";
 const char *topic = "starttopic";
+const char *diag_topic ="diagtopic";
 const int mqtt_port = 1883;
-int solar=A0;
-int ir_status=0;
-int buff[20]; 
-int buff_calibration[100];
-int diff=0;
-int a=0,b[20];
-int i=0;
-int c;
-int solar_flag=0;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
@@ -26,7 +18,6 @@ void setup() {
   // Set software serial baud to 115200;
   Serial.begin(115200);
    pinMode(LED, OUTPUT);
-    pinMode(solar,INPUT);
     pinMode(WIFI_LED,OUTPUT);
     digitalWrite(WIFI_LED,LOW);
    digitalWrite(LED,LOW);
@@ -37,13 +28,14 @@ void setup() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
       delay(500);
-     // Serial.println("Connecting to WiFi..");
+      Serial.println("Connecting to WiFi..");
   }
- // Serial.println("Connected to the WiFi network");
+  Serial.println("Connected to the WiFi network");
   digitalWrite(WIFI_LED,HIGH);
   //connecting to a mqtt broker
   client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
+  client.setCallback(diagcallback);
   while (!client.connected()) {
       String client_id = "esp8266-client-";
       client_id += String(WiFi.macAddress());
@@ -51,8 +43,6 @@ void setup() {
       if (client.connect(client_id.c_str())) {
            Serial.println("Public  mqtt broker connected");
           digitalWrite(WIFI_LED,HIGH);
-          
-
       } else {
         //  Serial.print("failed with state ");
           Serial.print(client.state());
@@ -61,6 +51,7 @@ void setup() {
   }
   // publish and subscribe
   client.subscribe("starttopic");
+  client.subscribe("diagtopic");
  
 }
 
@@ -73,34 +64,65 @@ void callback(char *topic, byte *payload, unsigned int length) {
   {
          Serial.print((char)payload[i]);
   }
-  solar_flag = 1;
+ Serial.println();
+  
+}
+void diagcallback(char *diag_topic, byte *payload, unsigned int length) {
+  int i;
+ Serial.print("Received [");
+  Serial.print(diag_topic);
+  Serial.print("]: ");
+  for (i = 0; i < length; i++)
+  {
+         Serial.print((char)payload[i]);
+  }
  Serial.println();
   
 }
 
 
 void loop() {
-  if(solar_flag==1)
-  {
-      solar_fun();
-  }
-  
-  
   client.loop();
- 
    if(Serial.available())
   {
     char rec = Serial.read();
-    solar_flag=0;
-       publishMessage(rec);
+    switch(rec)
+    {
+      case 0:
+      Training_mode_pub(0);
+      break;
+      case 1:
+      Training_mode_pub(1);
+      break;
+      case 2:
+      Diagnostic_mode_pub("DMT");
+      break;
+      case 3:
+      Diagnostic_mode_pub("DLT");
+      break;
+      case 4:
+      Diagnostic_mode_pub("DLS");
+      break;
+      default:
+      break;
 
+    }
 
   }
 }
-void publishMessage(char rec)
+// void wait(char data[])
+// {
+//   int inc=0;
+//   char arr[3];
+//   do{
+//       arr[inc++]=Serial.available();
+//   }while(arr != data);
+// }
+
+void Training_mode_pub(char rec)
 {
   StaticJsonDocument<200> doc;
-  doc["Status"] =rec;
+  doc["status"] =rec;
   char jsonBuffer[512];
   serializeJson(doc, jsonBuffer); // print to client
   client.publish("resulttopic", jsonBuffer);
@@ -109,38 +131,14 @@ void publishMessage(char rec)
   digitalWrite(BUILTIN_LED,LOW);
   delay(500);
 }
-
-void solar_fun()
+void Diagnostic_mode_pub(String rec)
 {
-  // digitalWrite(LED,LOW);
-     ir_status=analogRead(solar);
-      buff[i]=ir_status;
-      // Serial.print("Value: ");
-         Serial.println(buff[i]);
-         delay(500);
-     if(i>0)
-     {
-        c= buff[i]-buff[i-1];
-        // Serial.print("Value: ");
-        // Serial.println(c);
-         delay(50);
-        if(c>=12&&c<23)
-        {
-             digitalWrite(LED,HIGH);
-             Serial.println("Shot success");
-             delay(1000);
-                    }
-        else
-        {
-          digitalWrite(LED,LOW);
-        }
-        
-     }
-     if(i>19)
-        i=0;
-    i++;
+  StaticJsonDocument<200> doc;
+  doc["diagresult"] =rec;
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+  client.publish("diagresult", jsonBuffer);
 }
-
 
 
 
